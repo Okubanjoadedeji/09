ba62ea60115b398a63168bbef4c9779bbed3f54d
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-11-19 08:58:02 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Statement 
+
+const age = 16;
+
+if (age > 18) {
+  console.log("You're welcome!");
+} else if (age === 18) {
+  console.log("You just turned 18!");
+} else{
+  console.log("Get out!");
+}
